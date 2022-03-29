f9ba421b19d3f37af93a86736d932c33ba3185e0
DrakkarRyu <javisodst-metal@outlook.com>
2022-03-28 22:27:38 -0600

    Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Pokedex from './components/Pokedex';
+import PokemonInfo from './components/PokemonInfo';
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() {
 
@@ -7,7 +10,11 @@ function App() {
     <HashRouter>
       <div className="App">
         <Routes>
-          <Route path= "/" element={ <Login /> } />
+          <Route path= "/" element = { <Login /> } />
+          <Route element = {<ProtectedRoutes />}>
+            <Route path= '/Pokedex' element = { <Pokedex/>}/>
+            <Route path= '/Pokedex/:id' element = { <PokemonInfo/> }/>
+          </Route>
         </Routes>
       </div>
     </HashRouter>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const [ userName, setUserName ] = useState ("");
-
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const submit = e =>{
         e.preventDefault();
         console.log(userName);
+        dispatch({ type: "GET_USERNAME", payload: userName });
+        setUserName("");
+        navigate("/Pokedex");
     }
 
     return (
@@ -16,7 +22,8 @@ const Login = () => {
                 <input 
                 type = "text" 
                 value = {userName} 
-                onChange = { e => setUserName(e.target.value)} />
+                onChange = { e => setUserName(e.target.value)} 
+                required/>
                 <button>Submit</button>
             </form>
         </div>
