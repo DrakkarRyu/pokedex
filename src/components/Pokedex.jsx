798c748475f56ea6d8fec657b940e1958fd1be59
DrakkarRyu <javisodst-metal@outlook.com>
2022-03-28 22:42:41 -0600

    welcome message

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Pokedex = () => {
+
+    const userName = useSelector ( state => state.userName);
+
     return (
         <div>
             <h1>Pokedex</h1>
+            <p>Welcome {userName} those are the follow pokemons...</p>
         </div>
     );
 };
