06035ba8bdffb5488979e6f6f46e7571971ec634
DrakkarRyu <javisodst-metal@outlook.com>
2022-03-31 00:33:24 -0600

    filter working

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -9,11 +9,14 @@ const Pokedex = () => {
     const userName = useSelector(state => state.userName);
     const [pokemons, setPokemons] = useState([]);
     const [pokemonName, setPokemonName] = useState("");
+    const [types, setTypes]=useState([])
     const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1126`)
             .then(res => setPokemons(res.data.results))
+        axios.get(`https://pokeapi.co/api/v2/type`)
+            .then(res => setTypes(res.data.results))
     }, [])
 
     const submit = e =>{
@@ -21,10 +24,25 @@ const Pokedex = () => {
         navigate(`/Pokedex/${pokemonName}`);
     }
 
+    const handleType = e => {
+        axios.get(e.target.value)
+        .then(res => setPokemons(res.data.pokemon))
+    }
+
     return (
         <div>
             <h1>Pokedex</h1>
             <p>Welcome {userName} these are the follow pokemons...</p>
+            <div className='select'>
+                <select onChange={handleType}>
+                    <option value=''>Filter by pokemon type</option>
+                    {
+                        types.map(type=>(
+                            <option key={type.url} value={type.url}>{type.name}</option>
+                        ))
+                    }
+                </select>
+            </div>
             <form className='input-container' onSubmit={submit}>
                 <label htmlFor='pokemon-name'>Search by name</label>
                 <input type='text' id='pokemon-name' value={pokemonName} onChange={ e => setPokemonName(e.target.value)}/>
@@ -33,7 +51,7 @@ const Pokedex = () => {
             <ul>
                 {
                     pokemons.map(pokemon => (
-                        <PokemonCard pokemonURL={pokemon.url} key={pokemon.url} />
+                        <PokemonCard pokemonURL={pokemon.url ? pokemon.url : pokemon.pokemon.url} key={pokemon.url ? pokemon.url : pokemon.pokemon.url} />
                     ))
                 }
             </ul>
